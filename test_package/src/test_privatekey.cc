354b012faf1a215cdf7a70f4089379378675a1a0
Quinn <quinnwen.cn@gmail.com>
2025-04-28 11:04:45 +0800

    Fix compile error
    Signed-off-by: Quinn <quinnwen.cn@gmail.com>

--- test_package/src/test_privatekey.cc
+++ test_package/src/test_privatekey.cc
@@ -6,15 +6,6 @@
 
 using namespace Cryptix;
 
-class PrivateKeyTest : public ::testing::Test {
-protected:
-    void SetUp() override {
-        key_ = BaseKey::FromPrivateKeyContent(Utils::ReadFileContent(ROOT_PRIV_KEY));
-    }
-
-    UniqueEvpKey key_;
-};
-
 TEST(PrivateKeyTest, ConstructTest) {
     auto key = PrivateKey::FromKeyFile(ROOT_PRIV_KEY);
     EXPECT_TRUE(key.has_value());
@@ -29,25 +20,20 @@ TEST(PrivateKeyTest, ConstructTest) {
     EXPECT_TRUE(key2.has_value());
 }
 
-TEST_F(PrivateKeyTest, HandlesEmptyData) {
-    PrivateKey key(key_);
+TEST(PrivateKeyTest, HandlesEmptyData) {
+    auto key = PrivateKey::FromKeyFile(ROOT_PRIV_KEY);
     EXPECT_EQ(key.Sign("", SignAlgo::RSA_PKCS_V1_5), std::nullopt);
 }
 
-TEST_F(PrivateKeyTest, HandlesEmptyKey) {
-    PrivateKey key(nullptr);
-    EXPECT_EQ(key.Sign("test", SignAlgo::RSA_PKCS_V1_5), std::nullopt);
-}
-
-TEST_F(PrivateKeyTest, SignWithPkcs1V15) {
-    PrivateKey key(key_);
+TEST(PrivateKeyTest, SignWithPkcs1V15) {
+    auto key = PrivateKey::FromKeyFile(ROOT_PRIV_KEY);
     auto sig = key.Sign("test", SignAlgo::RSA_PKCS_V1_5);
     EXPECT_TRUE(sig.has_value());
     EXPECT_FALSE(sig.value().empty());
 }
 
-TEST_F(PrivateKeyTest, SignWithRsaSsaPss) {
-    PrivateKey key(key_);
+TEST(PrivateKeyTest, SignWithRsaSsaPss) {
+    auto key = PrivateKey::FromKeyFile(ROOT_PRIV_KEY);
     auto sig = key.Sign("test", SignAlgo::RSASSA_PSS);
     EXPECT_TRUE(sig.has_value());
     EXPECT_FALSE(sig.value().empty());
