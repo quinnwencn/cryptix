c28baf88a1bf4c94555920703d4d70a104aea5b4
Quinn <quinnwen.cn@gmail.com>
2025-04-28 11:07:42 +0800

    Fix again...
    Signed-off-by: Quinn <quinnwen.cn@gmail.com>

--- test_package/src/test_privatekey.cc
+++ test_package/src/test_privatekey.cc
@@ -22,19 +22,19 @@ TEST(PrivateKeyTest, ConstructTest) {
 
 TEST(PrivateKeyTest, HandlesEmptyData) {
     auto key = PrivateKey::FromKeyFile(ROOT_PRIV_KEY);
-    EXPECT_EQ(key.Sign("", SignAlgo::RSA_PKCS_V1_5), std::nullopt);
+    EXPECT_EQ(key.value().Sign("", SignAlgo::RSA_PKCS_V1_5), std::nullopt);
 }
 
 TEST(PrivateKeyTest, SignWithPkcs1V15) {
     auto key = PrivateKey::FromKeyFile(ROOT_PRIV_KEY);
-    auto sig = key.Sign("test", SignAlgo::RSA_PKCS_V1_5);
+    auto sig = key.value().Sign("test", SignAlgo::RSA_PKCS_V1_5);
     EXPECT_TRUE(sig.has_value());
     EXPECT_FALSE(sig.value().empty());
 }
 
 TEST(PrivateKeyTest, SignWithRsaSsaPss) {
     auto key = PrivateKey::FromKeyFile(ROOT_PRIV_KEY);
-    auto sig = key.Sign("test", SignAlgo::RSASSA_PSS);
+    auto sig = key.value().Sign("test", SignAlgo::RSASSA_PSS);
     EXPECT_TRUE(sig.has_value());
     EXPECT_FALSE(sig.value().empty());
 }
